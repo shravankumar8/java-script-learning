959f42e312fa4954b802948345fd842ef2beb6db
shravan <kumashravan5@gmail.com>
2023-06-09 00:04:06 +0530

    day 5 learning practive

---
+++ 04.a counter project.js/main.js
@@ -0,0 +1,17 @@
+var counter = document.querySelector(".counter");
+let followers = document.querySelector(".followers");
+
+let count = 0;
+
+setInterval(() => {
+  if (count < 1000) {
+    count++;
+    counter.innerHTML = count;
+  }
+  if(count==1000){
+    followers.innerHTML = (`${count} followers`)
+
+    
+}
+}, 1);
+
