84f311dd29fcf5342407b337aef25a6f1655329a
shravan lingampally <kumashravan5@gmail.com>
2023-07-07 23:43:11 +0530

    dom in js  revision W3S assignments

---
+++ 03.DOM/DOMlearning/main.js
@@ -0,0 +1,4 @@
+// document.getElementById("text2").style.color = "white";
+// document.getElementByClassName("text1").style.color = "white";
+
+// var elements = document.getElementsByClassName("text1")
